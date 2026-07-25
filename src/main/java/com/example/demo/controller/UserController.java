0230d04e14b3d0c8547e1e9907ec3d7d0d0620c3
123mansh <158152900+123mansh@users.noreply.github.com>
2026-07-25 19:08:50 +0530

    Added login and Officer registration functionality

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.dto.LoginRequestDTO;
+import com.example.demo.dto.LoginResponseDTO;
+import com.example.demo.dto.OfficerRegistrationDTO;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin("*")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO request) {
+
+        return userService.login(
+                request.getUname(),
+                request.getPassword());
+    }
+    @PostMapping("/registerOfficer")
+    public User registerOfficer(@RequestBody OfficerRegistrationDTO dto) {
+
+        return userService.registerOfficer(dto);
+
+    }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,58 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.LoginResponseDTO;
+import com.example.demo.dto.OfficerRegistrationDTO;
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+
+@Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public LoginResponseDTO login(String uname, String password) {
+
+        User user = userRepository.findByUnameAndPassword(uname, password);
+
+        if(user == null) {
+            throw new RuntimeException("Invalid Username or Password");
+        }
+
+        LoginResponseDTO response = new LoginResponseDTO();
+
+        response.setUid(user.getUid());
+        response.setRid(user.getRole().getRid());
+        response.setFullname(user.getFullname());
+
+        return response;
+    }
+    public User registerOfficer(OfficerRegistrationDTO dto) {
+
+        Role role = roleRepository.findById(4)
+                .orElseThrow(() -> new RuntimeException("Officer Role Not Found"));
+
+        User user = new User();
+
+        user.setUname(dto.getUname());
+        user.setPassword(dto.getPassword());
+        user.setFullname(dto.getFullname());
+        user.setAddress(dto.getAddress());
+        user.setPhone(dto.getPhone());
+        user.setEmail(dto.getEmail());
+
+        // Assign Officer Role
+        user.setRole(role);
+
+        return userRepository.save(user);
+    }
+    
 }
