b58d6d6ffe3e7a6d8e7e8eed65cd2a7762ba0777
123mansh <158152900+123mansh@users.noreply.github.com>
2026-07-31 16:27:28 +0530

    Initial Commit

---
+++ Backend/ComplaintService/src/main/java/com/example/demo/dto/StatusUpdateDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+public class StatusUpdateDTO {
+
+    private int cid;
+    private String status;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+    
+}
