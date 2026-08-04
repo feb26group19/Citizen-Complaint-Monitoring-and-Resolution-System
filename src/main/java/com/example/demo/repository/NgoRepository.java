ee8dc9bce7133f0554474cb1974414dff2fad2e1
Yashvini Malviya <145982717+yashvinimalviya@users.noreply.github.com>
2026-08-04 11:43:24 +0530

    Delete src directory

--- src/main/java/com/example/demo/repository/NgoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.Ngo;
-
-public interface NgoRepository extends JpaRepository<Ngo, Integer> {
-
-}
